3ae446b6a296a3226fba65b7c0edc680b90df7aa
Michael Byers <mikebyers24@gmail.com>
2024-04-24 14:08:59 -0400

    bcrypt

--- scripts/load-fake-data.ts
+++ scripts/load-fake-data.ts
@@ -1,6 +1,7 @@
 import { Client } from 'pg';
 import { loadEnvConfig } from '@next/env';
 import { faker } from '@faker-js/faker';
+import bcrypt from 'bcrypt';
 
 const projectDir = process.cwd();
 loadEnvConfig(projectDir);
@@ -20,9 +21,11 @@ async function loadFakeData(numUsers: number) {
   try {
     await client.query('begin');
     for (let index = 0; index < numUsers; index++) {
+      const saltRounds = 10;
+      const hash = await bcrypt.hash('password123', saltRounds);
       await client.query(
         'insert into public.users (username, password, avatar) values ($1, $2, $3)',
-        [faker.internet.userName(), 'password', faker.image.avatar()],
+        [faker.internet.userName(), hash, faker.image.avatar()],
       );
     }
 
