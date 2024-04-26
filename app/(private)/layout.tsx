e16615d7c5a90b71ed4777daabd6ab3940433d74
Michael Byers <mikebyers24@gmail.com>
2024-04-26 12:21:59 -0400

    private layout shell

--- app/(private)/layout.tsx
+++ app/(private)/layout.tsx
@@ -1,7 +1,17 @@
 import { ReactNode } from 'react';
+import Header from './Header';
+import Navbar from './Navbar';
+import Footer from './Footer';
 
 const PrivateLayout = ({ children }: { children: ReactNode }) => {
-  return <main>{children}</main>;
+  return (
+    <main>
+      <Header />
+      <Navbar />
+      <main>{children}</main>
+      <Footer />
+    </main>
+  );
 };
 
 export default PrivateLayout;
