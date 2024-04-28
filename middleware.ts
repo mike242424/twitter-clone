b0d6dbf52f009caaa33da723f21981bcd9d800f7
Michael Byers <mikebyers24@gmail.com>
2024-04-28 14:30:46 -0400

    auth middleware

---
+++ middleware.ts
@@ -0,0 +1,32 @@
+import { stat } from 'fs';
+import { jwtVerify } from 'jose';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function middleware(request: NextRequest) {
+  const pathname = request.nextUrl.pathname;
+
+  const authenticatedAPIRoutes = [pathname.startsWith('/api/users')];
+
+  if (authenticatedAPIRoutes.includes(true)) {
+    const cookie = request.cookies.get('jwt-token');
+
+    if (!cookie || !cookie?.value) {
+      return NextResponse.json({ error: 'unauthenticated' }, { status: 401 });
+    }
+
+    try {
+      const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+      await jwtVerify(cookie.value, secret);
+    } catch (error) {
+      console.log(error);
+      return NextResponse.json(
+        { erro: 'internal server error' },
+        { status: 500 },
+      );
+    }
+  }
+}
+
+export const config = {
+  matcher: '/:path*',
+};
