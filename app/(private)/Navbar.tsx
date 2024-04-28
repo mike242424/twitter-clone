9737b78effd2171dcc19a6f52a1f7a8d3350573c
Michael Byers <mikebyers24@gmail.com>
2024-04-28 18:09:50 -0400

    highlight active link.

--- app/(private)/Navbar.tsx
+++ app/(private)/Navbar.tsx
@@ -1,20 +1,47 @@
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 const Navbar = () => {
+  const pathname = usePathname();
+
   return (
     <nav className="bg-slate-600  p-3">
       <ul className="flex justify-around">
         <li className="hover:text-slate-900">
-          <Link href="/feed">Feed</Link>
+          <Link
+            className={pathname.startsWith('/feed') ? 'text-slate-900' : ''}
+            href="/feed"
+          >
+            Feed
+          </Link>
         </li>
         <li className="hover:text-slate-900">
-          <Link href="/profile">Profile</Link>
+          <Link
+            className={pathname.startsWith('/profile') ? 'text-slate-900' : ''}
+            href="/profile"
+          >
+            Profile
+          </Link>
         </li>
         <li className="hover:text-slate-900">
-          <Link href="/following">Following</Link>
+          <Link
+            className={
+              pathname.startsWith('/following') ? 'text-slate-900' : ''
+            }
+            href="/following"
+          >
+            Following
+          </Link>
         </li>
         <li className="hover:text-slate-900">
-          <Link href="/followers">Followers</Link>
+          <Link
+            className={
+              pathname.startsWith('/followers') ? 'text-slate-900' : ''
+            }
+            href="/followers"
+          >
+            Followers
+          </Link>
         </li>
       </ul>
     </nav>
