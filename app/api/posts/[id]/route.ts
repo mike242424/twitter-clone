270d363897465365177e13bccf9e7e3e55a5a34e
Michael Byers <mikebyers24@gmail.com>
2024-04-29 14:31:24 -0400

    get post endpoint.

---
+++ app/api/posts/[id]/route.ts
@@ -0,0 +1,20 @@
+import { getJWTPayload } from '@/app/utils/auth';
+import { sql } from '@/db';
+import { NextResponse } from 'next/server';
+
+export async function GET(
+  request: Request,
+  { params: { id } }: { params: { id: number } },
+) {
+  const jwtPayload = await getJWTPayload();
+  const res = await sql('select * from posts where id = $1 and user_id = $2', [
+    id,
+    jwtPayload.sub,
+  ]);
+
+  if (res.rowCount == 0) {
+    return NextResponse.json({ error: 'not found' }, { status: 404 });
+  }
+
+  return NextResponse.json({ data: res.rows[0] });
+}
