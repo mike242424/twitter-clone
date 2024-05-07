2fb32dca857aae663bf6921d94f33dfc2672725f
Michael Byers <mikebyers24@gmail.com>
2024-05-07 11:01:14 -0400

    search endpoint.

---
+++ app/api/search/route.ts
@@ -0,0 +1,20 @@
+import { sql } from '@/db';
+import { NextResponse } from 'next/server';
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const search = searchParams.get('q');
+  if (!search) {
+    return NextResponse.json(
+      { error: 'search parameter required' },
+      { status: 400 },
+    );
+  }
+
+  const res = await sql(
+    'select id, username, avatar from users where username ilike $1 limit $2',
+    [`%${search}%`, 10],
+  );
+
+  return NextResponse.json({ data: res.rows });
+}
