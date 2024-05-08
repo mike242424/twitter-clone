d000929ee91c65ec53ffb8cde2d3ac7d6603129e
Michael Byers <mikebyers24@gmail.com>
2024-05-08 12:23:52 -0400

    header loading.

--- app/(private)/Header.tsx
+++ app/(private)/Header.tsx
@@ -6,7 +6,24 @@ const Header = () => {
   const { data, error, isLoading } = useSWR('/api/users/profile');
 
   if (error) return <div>failed to load</div>;
-  if (isLoading) return <div>loading...</div>;
+  if (isLoading)
+    return (
+      <header className="flex flex-row w-full p-2 bg-slate-800 justify-between items-center">
+        <h1 className="text-3xl font-bold ml-11">Twitter Clone</h1>
+        <div className="mr-12">
+          <>
+            <div className="flex flex-col font-bold">
+              <div
+                className={`flex flex-row self-center gap-4 items-center my-4 ml-4`}
+              >
+                <div className="bg-slate-600 rounded-full h-[50px] w-[50px]"></div>
+                <p>Loading...</p>
+              </div>
+            </div>
+          </>
+        </div>
+      </header>
+    );
 
   return (
     <header className="flex flex-row w-full p-3 bg-slate-800 justify-between items-center">
--- app/components/FollowLoading.tsx
+++ app/components/FollowLoading.tsx
@@ -2,19 +2,17 @@ const FollowLoading = () => {
   const items = Array.from({ length: 10 }, (_, index) => index);
 
   return (
-    <div className="w-full">
-      <div className="grid sm:grid-cols-2 gap-4">
-        {items.map((item, i) => {
-          return (
-            <div
-              key={i}
-              className="bg-slate-200 rounded-lg text-slate-800 p-5 mx-4"
-            >
-              <div className="bg-slate-600 rounded-full h-[50px] w-[50px]"></div>
-            </div>
-          );
-        })}
-      </div>
+    <div className="grid sm:grid-cols-2 gap-4">
+      {items.map((item, i) => {
+        return (
+          <div
+            key={i}
+            className="bg-slate-200 rounded-lg text-slate-800 p-5 mx-4 sm:mx-0"
+          >
+            <div className="bg-slate-600 rounded-full h-[50px] w-[50px]"></div>
+          </div>
+        );
+      })}
     </div>
   );
 };
